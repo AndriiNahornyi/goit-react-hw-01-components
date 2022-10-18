839bbc16d945d9254b961d7d2218fa360e80406e
Andrii Nahornyi <108332521+AndriiNahornyi@users.noreply.github.com>
2022-10-18 13:57:11 +0300

    adds 2 task solution

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import {
+  Section,
+  Title,
+  StatList,
+  Item,
+  Label,
+  Percentagne,
+} from './Statistics.styled';
+
+export const Statistics = ({ title, stats }) => {
+  console.log('stats', stats);
+  return (
+    <Section>
+      {title && <Title>{title}</Title>}
+
+      <StatList>
+        {stats.map(item => (
+          <Item key={item.id}>
+            <Label>{item.label} </Label>
+            <Percentagne>{item.percentage}</Percentagne>
+          </Item>
+        ))}
+      </StatList>
+    </Section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
